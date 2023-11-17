0112fd6aa7687c08c2b2a1bd554fade0b1614ca2
Martin Stanček <martin.stancek123@gmail.com>
2023-11-17 10:01:48 +0100

    added handling for close popup + styles

--- src/js/info-popup.js
+++ src/js/info-popup.js
@@ -1,17 +1,23 @@
+document.querySelector('#close-popup-area').onclick = closeInfoPopup;
+
+
 function openInfoPopup(element) {
-  document.getElementById("popupInfo").style.display = "block";
+  document.getElementById("close-popup-area").style.display = "block";
   document.getElementById("tree-structrure-content").style.filter = "blur(6px)";
   document.getElementById("connector-lines").style.filter = "blur(6px)";
 
   targetNode = reakcia.filter(function(e){return e.name === element.parentElement.parentElement.id.replace(/-/g, " ")})[0];
   document.getElementById("info-popup-name").innerHTML = targetNode.name;
   document.getElementById("info-popup-popis").innerHTML = targetNode.description ? targetNode.description : "";
-  console.log(targetNode);
 
 }
 
-function closeInfoPopup() {
-  document.getElementById("popupInfo").style.display = "none";
+function closeInfoPopup(event) {
+  if(event && event.srcElement.id != 'close-popup-area') {
+    return;
+  }
+
+  document.getElementById("close-popup-area").style.display = "none";
   document.getElementById("tree-structrure-content").style.filter = "none";
   document.getElementById("connector-lines").style.filter = "none";
 
